3eea332c84635290411d2e70d49d5e21cf9412dc
Яков Власов <32434693+opanikita@users.noreply.github.com>
2019-03-19 16:05:29 +0300

    Я всё сломал

--- js/script.js
+++ js/script.js
@@ -95,3 +95,55 @@ window.addEventListener('DOMContentLoaded', function() {
 
         setClock('timer', deadline);
 });
+
+
+//modal
+
+let more = document.querySelector('.more'),
+    descrBtn = document.querySelectorAll('.description-btn'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
+
+    function showMoreInfo() {
+        for (let i = 0; i < descrBtn.length; i++) {
+            descrBtn[i].classList.add('more-splah');
+        }
+    }
+
+    showMoreInfo();
+
+    descrBtn.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('more-splash')) {
+            for (let i = 0; i < descrBtn.length; i++) {
+                if (target == descrBtn[i]) {
+                    showMoreInfo();
+                    break;
+                }
+            }
+        }
+    }
+
+    // descrBtn.addEventListener('click', function() {
+    //         overlay.style.display = 'block';
+    //         this.classList.add('more-splash');
+    //         document.body.style.overflow = 'hidden';
+    //     });  
+
+    // descrBtn.addEventListener('click', function() {
+    //     overlay.style.display = 'block';
+    //     this.classList.add('more-splash');
+    //     document.body.style.overflow = 'hidden';
+    // });    
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
