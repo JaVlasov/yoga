3dbd953246c9d21e44902245b9fc7ec654dad91a
Яков Власов <32434693+opanikita@users.noreply.github.com>
2019-03-22 03:20:20 +0300

    +

--- js/script.js
+++ js/script.js
@@ -132,3 +132,62 @@ close.addEventListener('click', function () {
     more.classList.remove('more-splash');
     document.body.style.overflow = '';
 });
+
+//Form
+
+let message = {
+    loading: 'Загрузка...',
+    success: 'Спасибо! Скоро мы с вами свяжемся!',
+    failure: 'Что-то пошло не так!'
+};
+
+let form = document.querySelector('.main-form'),
+    input = form.getElementsByTagName('input'),
+    statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    //проверка на ввод только цифр в строку с телефоном 
+
+    let inputFormNumber = document.querySelector('.popup-form__input'),
+    inp = inputFormNumber.value;
+    console.log(inp);
+
+    inputFormNumber.addEventListener = ('input', function(event) {
+        if (/\D/.inp(event.target.value)) {
+            event.target.value = '';
+        }
+    });
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Conttent-Type', 'application/json; charset=utf-8');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
