f4cf6c294685640647e731e7886b5456c978e64a
Яков Власов <32434693+opanikita@users.noreply.github.com>
2019-03-23 18:41:13 +0300

    +

--- js/script.js
+++ js/script.js
@@ -142,99 +142,72 @@ let message = {
 };
 
 let form = document.querySelector('.main-form'),
-    input = form.getElementsByTagName('input'),
+secondForm = document.querySelector('#form'),
+    input = document.getElementsByTagName('input'),
     statusMessage = document.createElement('div');
+
+    console.log(input);
 
     statusMessage.classList.add('status');
 
     //проверка на ввод только цифр в строку с телефоном 
 
-    let inputFormNumber = document.querySelector('.popup-form__input'),
-        contactFormNumber = document.getElementsByName('contphone')[0];
+    let inputFormNumber = document.querySelectorAll('input[name="phone"]');
+    
 
-        // console.log(contactFormNumber);
-
-        function validForm (event) {
-            if (/\D/.test(event.target.value)) {
-                event.target.value = '';
-            }
-        }
-
-    contactFormNumber.addEventListener('input', function (event) {
+for (let i = 0; i < inputFormNumber.length; i++) {
+    inputFormNumber[i].addEventListener('input', function(event) {
         if (/\D/.test(event.target.value)) {
             event.target.value = '';
         }
     });
-
-    inputFormNumber.addEventListener('input', function(event) {
-        if (/\D/.test(event.target.value)) {
-            event.target.value = '';
-        }
-    });
+}
+    
 
 function sendForm(elem) {
-    elem.addEventListener('submit', function(event) {
+    elem.addEventListener('submit', function (event) {
         event.preventDefault();
-        form.appendChild(statusMessage);
+        elem.appendChild(statusMessage);
         let formData = new FormData(elem);
 
-    function postData(data) {
+        function postData(data) {
 
-            return new Promise(function(resolve,reject) {
+            return new Promise(function (resolve, reject) {
                 let request = new XMLHttpRequest();
                 request.open('POST', 'server.php');
                 request.setRequestHeader('Conttent-Type', 'application/json; charset=utf-8');
-                
-                request.onreadystatechange = function() {
+
+                request.onreadystatechange = function () {
                     if (request.readyState < 4) {
                         resolve();
-                } else if (request.readyState === 4 && request.status == 200) {
+                    } else if (request.readyState === 4 && request.status == 200) {
                         resolve();
-                } else {
+                    } else {
                         reject();
                     }
                 }
-            }
-            request.send(data);
-            }
-            }
-
-            function clearInput() {
-                for (let i = 0; i < input.length; i++) {
-                    input[i].value = '';
-                }
-            }
-            }
-
-
-        
-        postData(formData)
-            .then(()=> statusMessage.innerHTML = message.loading)
-            .catch(()=> statusMessage.innerHTML = message.failure)
-            .then(clearInput);
-
-        // let formData = new FormData(form);
-
-        let obj = {};
-            formData.forEach(function(value, key) {
-                obj[key] = value;
+                request.send(data);
             });
-        let json = JSON.stringify(obj);
 
-        
-        
 
-        
-        // request.addEventListener('readystatechange', function() {
-        //     if (request.readyState < 4) {
-        //         statusMessage.innerHTML = message.loading;
-        //     } else if (request.readyState === 4 && request.status == 200) {
-        //         statusMessage.innerHTML = message.success;
-        //     } else {
-        //         statusMessage.innerHTML = message.failure;
-        //     }
-        // });
+        }
+        postData(formData)
+            .then(() => statusMessage.innerHTML = message.loading)
+            .then(() => statusMessage.innerHTML = message.success)
+            .catch(() => statusMessage.innerHTML = message.failure)
+            .then(clearInput);
+    });
 
-        // for (let i = 0; i < input.length; i++) {
-        //     input[i].value = '';
-        // }
+
+    function clearInput() {
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    }
+}
+
+
+
+sendForm(form);
+sendForm(secondForm);
+
