25cdd35d386bb5d9ac5740de7bbe7afd7361a169
Яков Власов <32434693+opanikita@users.noreply.github.com>
2019-03-27 09:45:39 +0300

    +

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -101,61 +101,59 @@ function calc() {
       personSum = 0,
       daysSum = 0,
       total = 0;
+  var cof;
   totalValue.innerHTML = 0;
-  var inputConter = document.querySelectorAll('.counter-block-input'); // проверка должна проверять, если введёт любой символ, который не в диапазоне [0-9] - не позволяет ввести. Почему у меня не работает? 
-
+  var inputConter = document.querySelectorAll('.counter-block-input');
   persons.addEventListener('change', function () {
     personSum = +this.value;
-
-    if (restDays.value !== '' && persons.value !== '') {
-      total = (daysSum + personSum) * 4000;
-    } else {
-      total = 0;
-    }
-
-    if (restDays.value == '' && restDays.value == 0) {
-      totalValue.innerHTML = 0;
-    } else {
-      totalValue.innerHTML = total;
-    }
+    changeTotal(restDays);
   });
   restDays.addEventListener('change', function () {
     daysSum = +this.value;
-
-    if (restDays.value !== '' && persons.value !== '') {
-      total = (daysSum + personSum) * 4000;
-    } else {
-      total = 0;
-    }
-
-    if (persons.value == '' || persons.value == 0) {
-      totalValue.innerHTML = 0;
-    } else {
-      totalValue.innerHTML = total;
-    }
+    changeTotal(persons);
   });
   place.addEventListener('change', function () {
+    cof = this.options[this.selectedIndex].value;
+    console.log(total);
+    console.log(cof);
+
     if (restDays.value == '' || persons.value == 0 || persons.value == '' || restDays.value == 0) {
       totalValue.innerHTML = 0;
     } else {
       var a = total;
-      totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+      console.log(a); // console.log('a',a, ' *cof', cof);
+
+      totalValue.innerHTML = a * cof;
     }
   });
 
   for (var i = 0; i < inputConter.length; i++) {
     inputConter[i].addEventListener('input', function (event) {
-      if (/\d/.test(event.target.value)) {
-        console.log('ok');
-      } else {
+      if (/\d/.test(event.target.value)) {} else {
         event.target.value = '';
       }
 
-      if (event.target.value == 0 || event.target.value == '') {
+      if (event.target.value == '0' || event.target.value == '') {
         total = 0;
         totalValue.textContent = 0;
       }
     });
+  }
+
+  function changeTotal(input) {
+    cof = place.options[place.selectedIndex].value;
+
+    if (restDays.value !== '' && persons.value !== '') {
+      total = (daysSum + personSum) * 4000;
+    } else {
+      total = 0;
+    }
+
+    if (input.value == '' && input.value == 0) {
+      totalValue.innerHTML = 0;
+    } else {
+      totalValue.innerHTML = total * cof;
+    }
   }
 }
 
